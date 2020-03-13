216321332a3af0d568782de9384469ad9382a822
Garrick Suemith <garrick.suemith@gmail.com>
2020-03-13 19:54:30 -0400

    add password functionality

--- borntuesday/flaskblog/__init__.py
+++ borntuesday/flaskblog/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9d552360b381a7bb3c740ad5eb301aa3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 from flaskblog import routes
