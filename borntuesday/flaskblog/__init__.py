de57de2c08c59ccda6332245d8813c956c338bcf
Garrick Suemith <garrick.suemith@gmail.com>
2020-03-13 20:47:41 -0400

    add logout route

--- borntuesday/flaskblog/__init__.py
+++ borntuesday/flaskblog/__init__.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '9d552360b381a7bb3c740ad5eb301aa3'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
 
 from flaskblog import routes
